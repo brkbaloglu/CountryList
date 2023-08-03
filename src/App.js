60359375c1d7b54c830ce2143f5874cb4693cad3
Burak Baloglu <burakbaloglu148@gmail.com>
2023-08-04 01:13:22 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [countries,setCountries] = useState([])
+  const url = "https://restcountries.com/v3.1/all";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCountries(response.data)
+    }).catch(error => console.log(error))
+  })
+
+   console.log(countries)
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='grid grid-cols-5 py-6 px-10 border-b-2 border-black sticky top-0 z-50 bg-white'>
+        <h2 className='text-center text-xl font-bold'>Bayrak</h2>
+        <h2 className='text-center text-xl font-bold'>İsim</h2>
+        <h2 className='text-center text-xl font-bold'>Başkent</h2>
+        <h2 className='text-center text-xl font-bold'>Saat Dilimi</h2>
+        <h2 className='text-center text-xl font-bold'>Ülke Kodu</h2>
+      </div>
+      {countries.map(country => {
+        return(
+          <div key={country.name.common} className='grid grid-cols-5 py-6 px-10 border-b-2 border-gray-300'>
+            <img src={country.flags.png} alt="" />
+            <h2 className='text-center'>{country.name.common}</h2>
+            <h4 className='text-center'>{country.capital}</h4>
+            <h4 className='text-center '>{country.timezones[0]}</h4>
+            <h4 className='text-center'>{country.cca2}</h4>
+          </div>
+        )
+      })}
     </div>
   );
 }
